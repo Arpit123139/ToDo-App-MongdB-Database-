7aee07e1f15db0840be601d7f7e3eab9cb2039d6
Arpit123139 <arpitgajya2001@gmail.com>
2022-10-06 21:39:13 +0530

    Created a Navigation Drawer

---
+++ app/src/main/java/com/example/todoapp/HomeFragment.java
@@ -0,0 +1,43 @@
+package com.example.todoapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class HomeFragment extends Fragment {
+    FloatingActionButton floatingActionButton;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        floatingActionButton =view.findViewById(R.id.add_task_btn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
+        return view;
+    }
+
+    private void showAlertDialog() {
+
+    }
+}
